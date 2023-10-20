e22325028f4d3506b4c82ef69518a99cec2b5455
lenchq <tirr06@gmail.com>
2023-10-21 02:37:48 +0300

    delete

--- src/app/errors/page.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from 'react';
- 
-const Errors: FunctionComponent = () => {
-    return ( <h1>errors</h1> );
-}
- 
-export default Errors;
--- src/app/recent/page.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from 'react';
- 
-const RecentErrors: FunctionComponent = () => {
-    return ( <h1>recent</h1> );
-}
- 
-export default RecentErrors;
