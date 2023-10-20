3c5c65a49abde80654f7b897a40e2c1c6278be46
lenchq <tirr06@gmail.com>
2023-10-21 02:35:45 +0300

    tochka

---
+++ src/app/errors/page.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+ 
+const Errors: FunctionComponent = () => {
+    return ( <h1>errors</h1> );
+}
+ 
+export default Errors;
---
+++ src/app/recent/page.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+ 
+const RecentErrors: FunctionComponent = () => {
+    return ( <h1>recent</h1> );
+}
+ 
+export default RecentErrors;
