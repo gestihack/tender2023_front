b8f38e10f0c240d3d89b7296f87daf109033b3ef
lenchq <tirr06@gmail.com>
2023-10-21 11:54:04 +0300

    gdsjg

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 
 </script>
-<body class="bg-[#E6E5FF]">
+<section class="bg-[#E6E5FF]">
     <h1>Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
     <div class="p-2"></div>
-</body>
+</section>
 
 <style>
 </style>
